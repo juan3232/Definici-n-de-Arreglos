6a73778c9eff1c0efc536c7b1a4754e92a7eb304
Juan  Hernandez <72632179+juan3232@users.noreply.github.com>
2022-03-13 10:07:51 -0600

    Add files via upload

---
+++ ejer/src/App.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int[] Numeros = new int[3];
+
+        Numeros[0] = 3;
+        Numeros[1] = 2;
+        Numeros[2] = 3;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(Numeros[i]);
+
+        }
+
+    }
+}
